0baf9c26ba595e3d5aa4bb586ceb2299fcfe5c79
Ilyukina <anyailyukina@yandex.ru>
2023-02-05 15:25:44 +0800

    Инициализация проекта, подключение tg api

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-page">
+    <button
+      class="close-button"
+      @click="close"
+      >
+      Закрыть
+    </button>
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const tg = window.Telegram.WebApp
+    // приложение проинициализировалось
+    tg.ready()
+
+    console.log(tg, 'tg')
+    const close = () => {
+      console.log('close')
+      tg.close()
+    }
+    return {
+      close
+    }
+
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
